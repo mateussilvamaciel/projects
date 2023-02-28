d2d1a3e9e02e56672d571c40465898f459dc4f4a
Mateus Silva Maciel <mateussilvamaciel+1@gmail.com>
2023-02-27 21:29:43 -0300

    Tarefa das horas

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
-leg_a = float(input("Input first leg length: "))
-leg_b = float(input("Input second leg length: "))
-print("Hypotenuse length is", (leg_a**2 + leg_b**2) ** .5)
+hour = int(input("Starting time (hours): "))
+mins = int(input("Starting time (minutes): "))
+dura = int(input("Event duration (minutes): "))
+
+# Write your code here.
+minutos = mins + dura
+
+rest = minutos % 60
+horas = int(hour + (dura+rest)/60) 
+horast = int(hour + (horas+rest)/12) 
+val =(mins // 60)
+
+# 1print(rest)
+print("O evento começou às ", hour, ":", mins," com a duração de ", dura, " minutos", sep="")
+print("O evento terminou às ", horas, ":", rest, sep="")
+# print(horas)
