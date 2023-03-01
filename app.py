950441a6a856f35a770d72eab093d468da264e6c
Mateus Silva Maciel <mateussilvamaciel+1@gmail.com>
2023-02-28 21:12:09 -0300

    Alteração tarefa horas

--- app.py
+++ app.py
@@ -2,15 +2,19 @@ hour = int(input("Starting time (hours): "))
 mins = int(input("Starting time (minutes): "))
 dura = int(input("Event duration (minutes): "))
 
+
+minsT=  mins + dura + (60*hour)
 # Write your code here.
+horasT =
 minutos = mins + dura
 
 rest = minutos % 60
 horas = int(hour + (dura+rest)/60) 
-horast = int(hour + (horas+rest)/12) 
-val =(mins // 60)
+horast = int((horas+rest)/12) 
+#val =(mins // 60)
 
 # 1print(rest)
-print("O evento começou às ", hour, ":", mins," com a duração de ", dura, " minutos", sep="")
-print("O evento terminou às ", horas, ":", rest, sep="")
-# print(horas)
+# print("O evento começou às ", hour, ":", mins," com a duração de ", dura, " minutos", sep="")
+# print("O evento terminou às ", horas, ":", rest, sep="")
+# print(horast)
+print(minsT)
