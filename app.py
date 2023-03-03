6bdcb6ff049b0131075b6da07e7c0b4ac3857732
Mateus Silva Maciel <mateussilvamaciel@gmail.com>
2023-03-02 22:28:41 -0300

    Tarefa de horas concluida

--- app.py
+++ app.py
@@ -1,20 +1,13 @@
 hour = int(input("Starting time (hours): "))
 mins = int(input("Starting time (minutes): "))
 dura = int(input("Event duration (minutes): "))
-
-
-minsT=  mins + dura + (60*hour)
 # Write your code here.
-horasT =
-minutos = mins + dura
 
-rest = minutos % 60
-horas = int(hour + (dura+rest)/60) 
-horast = int((horas+rest)/12) 
-#val =(mins // 60)
+Totalmins=   mins + dura + (60*hour) #Transforma tudo em minutos
 
-# 1print(rest)
-# print("O evento começou às ", hour, ":", mins," com a duração de ", dura, " minutos", sep="")
-# print("O evento terminou às ", horas, ":", rest, sep="")
-# print(horast)
-print(minsT)
+hr = (Totalmins//60) % 24 #encontra o total de horas de 0 a 23
+
+min = (Totalmins - (hr*60)) % 60 #encontra o total de minutos de 0 a 59
+
+print("O evento começou às ", hour, ":", mins," com a duração de ", dura, " minutos", sep="")
+print("O evento terminou às ", hr, ":", min, sep="")
