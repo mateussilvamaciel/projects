3fcf3ba2a5c97aa034e4923af9315a706a714e57
Mateus Silva Maciel <mateussilvamaciel@gmail.com>
2023-03-08 12:00:21 -0300

    Tarefa do curso

---
+++ app1.py
@@ -0,0 +1,8 @@
+phase= str(input("Digite a palavras: "))
+
+if phase == "Spathiphyllum":
+    print("Yes - Spathiphyllum is the best plant ever!")
+elif phase == "spathiphyllum":
+    print("No, I want a big Spathiphyllum!")
+else:
+    print("Spathiphyllum! Not [input]!")
