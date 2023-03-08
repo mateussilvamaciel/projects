e9b70056c9f1a829cdf90350705f65520f7a9e2b
Mateus Silva Maciel <mateussilvamaciel@gmail.com>
2023-03-08 12:07:55 -0300

    ajustes

--- app1.py
+++ app1.py
@@ -1,4 +1,8 @@
-<<<<<<< HEAD
+#prints the sentence "Yes - Spathiphyllum is the best 
+#prints "No, I want a big Spathiphyllum!" if the inputted string is "spathiphyllum" (lower-case)
+#prints "Spathiphyllum! Not [input]!" otherwise. Note: [input] is the string taken as input.
+#Test your code using the data we've provided for you. And get yourself a Spathiphyllum, too!
+
 phase= str(input("Digite a palavras: "))
 
 if phase == "Spathiphyllum":
@@ -7,12 +11,3 @@ elif phase == "spathiphyllum":
     print("No, I want a big Spathiphyllum!")
 else:
     print("Spathiphyllum! Not [input]!")
-=======
-# Comparison operators and conditional execution
-#Write a program that utilizes the concept of conditional execution, takes a string as input, and:
-
-#prints the sentence "Yes - Spathiphyllum is the best 
-#prints "No, I want a big Spathiphyllum!" if the inputted string is "spathiphyllum" (lower-case)
-#prints "Spathiphyllum! Not [input]!" otherwise. Note: [input] is the string taken as input.
-#Test your code using the data we've provided for you. And get yourself a Spathiphyllum, too!
->>>>>>> 0218010e98866165b59d2b7a94499a3ca26029e2
