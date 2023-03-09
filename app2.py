1792d3beeb513ca045a1f023c03210bc0b8ae9be
Mateus Silva Maciel <mateussilvamaciel@gmail.com>
2023-03-09 12:16:31 -0300

    Finalizada tarefa

--- app2.py
+++ app2.py
@@ -5,11 +5,14 @@
 
 income = float(input("Enter the annual income: "))
 
-if int(income) < 85528:
-	tax = income * 0.18 - 556.02
+if income < 85528:
+	tax = float(income * 0.18 - 556.02)
 else:
-    tax = 14839.02 + 0.32(income - 85528)
-# Write the rest of your code here.
+    tax = float(14839.02 + 0.32*(income - 85528))
+
+if tax <= 0:
+      tax = 0
+#Write the rest of your code here.
 
 tax = round(tax, 0)
 print("The tax is:", tax, "thalers")
