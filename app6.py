7e0e684eefbbe0e187a4640e6bc2982bd261f546
Mateus Silva Maciel <mateussilvamaciel@gmail.com>
2023-03-16 10:41:49 -0300

    new task

---
+++ app6.py
@@ -0,0 +1,19 @@
+user_word = str(input("Please, write a word: "))
+user_word = user_word.upper()
+# Prompt the user to enter a word
+# and assign it to the user_word variable.
+
+for letter in user_word:
+    if letter == "A":
+        continue   
+    elif letter == "E":
+        continue
+    elif letter == "I":
+        continue
+    elif letter == "O":
+        continue
+    elif letter == "U":
+        continue
+    else:
+        print(letter)
+
