61b5f4a194071a6936ba77fd28921616dd68b6cf
Mateus Silva Maciel <mateussilvamaciel@gmail.com>
2023-03-16 10:03:50 -0300

    About loop in Python

---
+++ app5.py
@@ -0,0 +1,10 @@
+word = str(input("Write any word: "))
+
+while True:
+    if word == "chupacabra":
+        break
+    else:
+        word = input("Write other word: ")
+    
+    
+print("You've successfully left the loop.")
