c04e07f9f480c47eafa1bcf66c805c3ff70704fc
Mateus Silva Maciel <mateussilvamaciel@gmail.com>
2023-03-25 15:00:37 -0300

    Adicionando nova tarefa

---
+++ app7.py
@@ -0,0 +1,10 @@
+numbers = [10, 5, 7, 2, 1]
+print("Original list contents:", numbers)  # Printing original list contents.
+
+numbers[0] = 111
+print("\nPrevious list contents:", numbers)  # Printing previous list contents.
+
+numbers[1] = numbers[4]  # Copying value of the fifth element to the second.
+print("Previous list contents:", numbers)  # Printing previous list contents.
+
+print("\nList length:", len(numbers))  # Printing the list's length.
