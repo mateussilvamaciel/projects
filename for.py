897e9e5686cf9d2651993032250266471e386f53
Mateus Silva Maciel <mateus.dti@uniube.br>
2023-06-22 21:57:04 -0300

    aff

--- for.py
+++ for.py
@@ -1,6 +1,9 @@
 # Loop For
-for i in range(5):
+for i in range(5):# Argumento tamanho do array
     print("O valo de i é atualmente", i)
 
-for i in range(2, 8):
+for i in range(2, 8):# primeiro argumento- Valor inicial é o inicio, segundo é o tamanho
+    print("O valor de i é atualmente", i)
+
+for i in range(2, 10, 3):# primeiro argumento- Valor inicial é o inicio, segundo é o tamanho e terceiro é um incremento
     print("O valor de i é atualmente", i)
