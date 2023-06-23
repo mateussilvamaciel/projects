f46ec36c497e7f38dc82da4937e34cf068ba38c5
Mateus Silva Maciel <mateus.dti@uniube.br>
2023-06-22 21:54:19 -0300

    Travei aqui nesta potencianção

---
+++ for.py
@@ -0,0 +1,6 @@
+# Loop For
+for i in range(5):
+    print("O valo de i é atualmente", i)
+
+for i in range(2, 8):
+    print("O valor de i é atualmente", i)
---
+++ teste.py
@@ -0,0 +1,4 @@
+power = 2
+for expo in range(10):
+  print("2 à potência de", expo, "é", power)
+  power = power**expo
